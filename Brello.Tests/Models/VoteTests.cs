aa5e5ec7389ef3b83c83f2e09f055d252fed2cea
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-26 19:16:46 -0700

    updated project files

---
+++ Brello.Tests/Models/VoteTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brello.Models;
+
+namespace Brello.Tests.Models
+{
+    [TestClass]
+    public class VoteTests
+    {
+        [TestMethod]
+        public void EnsureICanCreateVoteClassInstance()
+        {
+            Vote vote = new Vote();
+            Assert.IsNotNull(vote);
+        }
+    }
+}

2c7d20ff47b7fee98ce1779dd54d489e812423f8
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-28 18:06:31 -0700

    how to implement comparison operators

---
+++ Brello.Tests/Models/ColorTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brello.Models;
+
+namespace Brello.Tests.Models
+{
+    [TestClass]
+    public class ColorTests
+    {
+        [TestMethod]
+        public void ColorEnsureICanCreateInstance()
+        {
+            Color c = new Color();
+            Assert.IsNotNull(c);
+        }
+
+        [TestMethod]
+        public void ColorEnsurePropertiesWork()
+        {
+            // Object Initializer syntax
+            Color c = new Color {Name = "Blue", Value = "#0000ff"};
+            // Otherwise you'd have to
+            /*
+            Color c = new Color();
+            c.Name = "Blue";
+            c.Value = "#0000ff";
+            */
+            Assert.AreEqual("Blue", c.Name);
+            Assert.AreEqual("#0000ff", c.Value);
+        }
+
+        [TestMethod]
+        public void ColorEnsureColorsAreEqual()
+        {
+            Color color1 = new Color { Name = "Blue", Value = "#0000ff" };
+            Color color2 = new Color { Name = "Blue", Value = "#0000ff" };
+            Assert.AreEqual(0, color1.CompareTo(color2));
+            Assert.IsTrue(color1 == color2);
+        }
+
+        [TestMethod]
+        public void ColorEnsureColorsAreNotEqual()
+        {
+            Color color1 = new Color { Name = "Blue", Value = "#0000ff" };
+            Color color2 = new Color { Name = "Red", Value = "#ff0000" };
+            Assert.IsTrue(0 != color1.CompareTo(color2));
+            Assert.IsTrue(color1 != color2);
+        }
+
+        [TestMethod]
+        public void ColorEnsureGreaterThanComparison()
+        {
+            Color color1 = new Color { Name = "Blue", Value = "#0000ff" };
+            Color color2 = new Color { Name = "Black", Value = "#000000" };
+            Assert.AreEqual(1, color1.CompareTo(color2));
+            Assert.IsTrue(color1 > color2);
+        }
+
+        [TestMethod]
+        public void ColorEnsureLessThanComparison()
+        {
+            Color color1 = new Color { Name = "Blue", Value = "#0000ff" };
+            Color color2 = new Color { Name = "Red", Value = "#ff0000" };
+            Assert.AreEqual(-1, color1.CompareTo(color2));
+            Assert.IsTrue(color1 < color2);
+        }
+    }
+}
--- Brello/Models/Color.cs
+++ Brello/Models/Color.cs
@@ -5,10 +5,39 @@ using System.Web;
 
 namespace Brello.Models
 {
-    public class Color
+    public class Color : IComparable
     {
         public int ColorId { get; }
         public string Name { get; set; }
         public string Value { get; set; }
+        
+        public int CompareTo(object obj)
+        {
+            Color other_color = obj as Color;
+            // Other way to cast
+            // Color other_color = (Color)obj;
+            return this.Name.CompareTo(other_color.Name);
+
+        }
+        
+        public static bool operator==(Color color1,object obj2)
+        {
+            return 0 == color1.CompareTo(obj2 as Color);
+        }
+
+        public static bool operator !=(Color color1, object obj2)
+        {
+            return 0 != color1.CompareTo(obj2 as Color); ;
+        }
+
+        public static bool operator >(Color color1, object obj2)
+        {
+            return 1 == color1.CompareTo(obj2 as Color);
+        }
+
+        public static bool operator <(Color color1, object obj2)
+        {
+            return -1 == color1.CompareTo(obj2 as Color);
+        }
     }
 }

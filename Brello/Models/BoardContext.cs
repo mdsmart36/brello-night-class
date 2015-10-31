64d54cf26be84d469c9373548689e78fa616b29e
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-31 07:58:04 -0700

    Board Service and Context classes

---
+++ Brello/Models/BoardContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Brello.Models
+{
+    public class BoardContext : DbContext
+    {
+        // Uses the connection string from the Web.config named "BoardContext"
+        //public BoardContext() : base("name=BoardContext") { }
+
+        public virtual IDbSet<Color> Colors { get; set; }
+        public virtual IDbSet<Card> Cards { get; set; }
+    }
+}
---
+++ Brello/Models/BoardService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Brello.Models
+{
+    public class BoardService
+    {
+        private BoardContext context;
+        public BoardService(BoardContext _context) {
+            context = _context;
+        }
+    }
+}

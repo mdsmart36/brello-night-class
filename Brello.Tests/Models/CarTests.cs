e33b4f1497ca90f3ff14820dae8f9c2318839cbe
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-31 11:58:32 -0700

    Unused class to help explore use of Mocking with Moq.

---
+++ Brello.Tests/Models/CarTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brello.Models;
+using Moq;
+
+namespace Brello.Tests.Models
+{
+    [TestClass]
+    public class CarTests
+    {
+        [TestMethod]
+        public void CarEnsureICanCreateInstance()
+        {
+            Car mycar = new Car();
+            Assert.IsNotNull(mycar);
+        }
+
+        [TestMethod]
+        public void CarEnsureMyCarCanHonk()
+        {
+            Car mycar = new Car();
+            Assert.AreEqual("HONK!", mycar.Horn());
+        }
+
+        [TestMethod]
+        public void CarEnsureICanMockMyCarHorn()
+        {
+            Mock<Car> mock_car = new Mock<Car>();
+            mock_car.Setup(x => x.Horn()).Returns("BEEP!");
+            Assert.AreEqual("BEEP!", mock_car.Object.Horn());
+        }
+
+        [TestMethod]
+        public void CarEnsureICanMockInterface()
+        {
+            // Honk() was called atleast once
+            Mock<ICar> mock_car = new Mock<ICar>();
+            // This is invalid: var myvar = new ICar();
+            mock_car.Setup(x => x.Honk()).Returns("BEEP!");
+            Assert.AreEqual("BEEP!", mock_car.Object.Honk());
+            mock_car.Verify(x => x.Honk(), Times.Never);
+        }
+
+        /*
+        //Moq Requires methods to be called explicitly after setup.
+        
+        [TestMethod]
+        public void CarEnsureReadyEngineIsCalled()
+        {
+            Mock<Car> mock_car = new Mock<Car>();
+            
+            mock_car.Setup(x => x.ReadyEngines());
+            mock_car.Object.Horn();
+            //mock_car.Setup(x => x.Horn()).Returns("BEEP!");
+            //Assert.AreEqual("BEEP!", mock_car.Object.Horn());
+            mock_car.Verify(x => x.ReadyEngines(), Times.Once());
+        }
+        */
+        
+    }
+}
---
+++ Brello/Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Brello.Models
+{
+    public interface ICar
+    {
+        string Honk();
+    }
+
+    public class Car
+    {
+        // In order for Mock to change behavior, Method must be virtual.
+        public virtual string Horn()
+        {
+            ReadyEngines();
+            return "HONK!";
+        }
+
+        // Even when counting method calls, methods should be virtual
+        public virtual void ReadyEngines() { }
+    }
+}

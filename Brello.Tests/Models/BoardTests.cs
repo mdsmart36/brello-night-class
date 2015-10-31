68155f7140ddf8590749b8dd2a44becaaefb73b0
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-31 12:00:07 -0700

    Basic tests and board class

---
+++ Brello.Tests/Models/BoardTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brello.Models;
+
+namespace Brello.Tests.Models
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void BoardEnsureICanCreateInstance()
+        {
+            Board board = new Board();
+            Assert.IsNotNull(board);
+        }
+
+        [TestMethod]
+        public void BoardEnsurePropertiesWork()
+        {
+            //Color color = new Color { Name = "Blue", Value = "#0000ff" };
+            // Object Initializer syntax
+            Board board = new Board();
+            Assert.AreEqual("My Board", board.Title);
+            Assert.AreEqual(0, board.Lists.Count);
+            Assert.AreEqual(0, board.Followers.Count);
+        }
+    }
+}
---
+++ Brello/Models/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Brello.Models
+{
+    public class Board
+    {
+        public int BoardId { get; set; }
+        public string Title { get; set; }
+        public virtual ICollection<BrelloList> Lists { get; set; }
+        public virtual ICollection<ApplicationUser> Followers { get; set; }
+    }
+}

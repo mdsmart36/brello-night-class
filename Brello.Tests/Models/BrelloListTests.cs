dd50da627c3b099ee064c43164f982a2ed59c0cd
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-10-31 12:01:57 -0700

    Basic tests and BrelloList class

---
+++ Brello.Tests/Models/BrelloListTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brello.Models;
+
+namespace Brello.Tests.Models
+{
+    [TestClass]
+    public class BrelloListTests
+    {
+        [TestMethod]
+        public void BrelloListEnsureICanCreateInstance()
+        {
+            BrelloList brello_list = new BrelloList();
+            Assert.IsNotNull(brello_list);
+        }
+    }
+}

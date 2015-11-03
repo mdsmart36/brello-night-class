7d76f642b6ca3065842d8c483f4a7baf3d3a7ba7
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-11-02 19:36:44 -0800

    In-class implementation of BrelloLists model and properties test

--- Brello.Tests/Models/BrelloListTests.cs
+++ Brello.Tests/Models/BrelloListTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Brello.Models;
+using System.Collections.Generic;
 
 namespace Brello.Tests.Models
 {
@@ -12,6 +13,20 @@ namespace Brello.Tests.Models
         {
             BrelloList brello_list = new BrelloList();
             Assert.IsNotNull(brello_list);
+        }
+
+        [TestMethod]
+        public void BrelloListEnsurePropertiesWork()
+        {
+            Card card1 = new Card { Title = "My Card" };
+            List<Card> list_of_cards = new List<Card>();
+            list_of_cards.Add(card1);
+            BrelloList list = new BrelloList { Title = "My List",
+                                               BrelloListId = 1,
+                                               CreatedAt = DateTime.Parse("2015-01-02"),
+                                               Cards = list_of_cards };
+            Assert.AreEqual("My List", list.Title);
+            Assert.AreEqual(1, list.Cards.Count);
         }
     }
 }
--- Brello/Models/BrelloList.cs
+++ Brello/Models/BrelloList.cs
@@ -7,5 +7,9 @@ namespace Brello.Models
 {
     public class BrelloList
     {
+        public int BrelloListId { get; set; }
+        public string Title { get; set; }
+        public List<Card> Cards { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }

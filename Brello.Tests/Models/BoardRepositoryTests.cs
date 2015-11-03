b373ce8ed36eeb82ad2c41d65b3c55b4c1ff193c
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-11-02 19:40:41 -0800

    In-class implmentation of CreateBoard repository method with Mocking. Renamed file from BoardService to BoardRepository

---
+++ Brello.Tests/Models/BoardRepositoryTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brello.Models;
+using Moq;
+using System.Data.Entity;
+
+namespace Brello.Tests.Models
+{
+    [TestClass]
+    public class BoardRepositoryTests
+    {
+        [TestMethod]
+        public void BoardRepositoryEnsureICanCreateInstance()
+        {
+            var some_context = new Mock<BoardContext>();
+            BoardRepository board = new BoardRepository(some_context.Object);
+            Assert.IsNotNull(board);
+        }
+        
+        [TestMethod]
+        public void BoardRepositoryEnsureICanAddAList()
+        {
+            var some_context = new Mock<BoardContext>();
+            BoardRepository board_repo = new BoardRepository(some_context.Object);
+            BrelloList list = new BrelloList();
+            Board board = new Board();
+
+            bool actual = board_repo.AddList(board, list);
+
+            Assert.AreEqual(1, board_repo.GetAllLists().Count);
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void BoardRepositoryEnsureThereAreZeroLists()
+        {
+            var some_context = new Mock<BoardContext>();
+            BoardRepository board_repo = new BoardRepository(some_context.Object);
+            
+            int expected = 0;
+            int actual = board_repo.GetAllLists().Count;
+            Assert.AreEqual(expected, actual);
+            
+        }
+
+        // These tests are telling us to start looking at
+        // How to define CRUD operations for Boards
+        // Why? Because a List cannot exists without a Board
+        [TestMethod]
+        public void BoardRepositoryEnsureABoardHasZeroLists()
+        {
+            var some_context = new Mock<BoardContext>();
+            BoardRepository board_repo = new BoardRepository(some_context.Object);
+            Board board = new Board();
+            int expected = 0;
+            Assert.AreEqual(expected, board_repo.GetAllLists(board).Count);
+        }
+
+        [TestMethod]
+        public void BoardRepositoryCanGetABoard()
+        {
+
+        }
+
+        [TestMethod]
+        public void BoardRepositoryCanCreateBoard()
+        {
+            var mock_context = new Mock<BoardContext>();
+            var mock_boards = new Mock<DbSet<Board>>();
+            // One way to call an object underneath a mock.
+            //mock_context.Object.Boards
+
+            mock_context.Setup(m => m.Boards).Returns(mock_boards.Object);
+
+            BoardRepository board_repo = new BoardRepository(mock_context.Object);
+            string title = "My Awesome Board";
+            ApplicationUser owner = new ApplicationUser();
+            Board added_board = board_repo.CreateBoard(title, owner);
+            Assert.IsNotNull(added_board);
+
+            mock_boards.Verify(m => m.Add(It.IsAny<Board>()));
+            mock_context.Verify(x => x.SaveChanges(), Times.Once());
+        }
+        
+    }
+}
--- Brello/Models/BoardRepository.cs
+++ Brello/Models/BoardRepository.cs
@@ -6,10 +6,10 @@ using System.Data.Entity;
 
 namespace Brello.Models
 {
-    public class BoardService
+    public class BoardRepository
     {
         private BoardContext context;
-        public BoardService(BoardContext _context) {
+        public BoardRepository(BoardContext _context) {
             context = _context;
         }
 
@@ -28,6 +28,15 @@ namespace Brello.Models
         public List<BrelloList> GetAllLists(Board _board)
         {
             return null;
+        }
+
+        public Board CreateBoard(string title, ApplicationUser owner)
+        {
+            Board my_board = new Board { Title = title, Owner = owner};
+            context.Boards.Add(my_board);
+            context.SaveChanges(); // This saves something to the Database
+
+            return my_board;
         }
     }
 }

a5206b4bfc642d1f3a349525fbb4ed05682cd790
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-11-07 12:11:55 -0800

    Implemented GetBoards Method

--- Brello/Models/BoardRepository.cs
+++ Brello/Models/BoardRepository.cs
@@ -52,5 +52,11 @@ namespace Brello.Models
             
             return query.Count();
         }
+
+        public List<Board> GetBoards(ApplicationUser user1)
+        {
+            var query = from b in context.Boards where b.Owner == user1 select b;
+            return query.ToList<Board>(); // Same as query.ToList();
+        }
     }
 }

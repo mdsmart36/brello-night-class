bb29b30efe6bf22fd3931885ea8e9bfa3385ff3d
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-12-05 10:31:15 -0800

    new JS file

--- Brello/App_Start/BundleConfig.cs
+++ Brello/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace Brello
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
---
+++ Brello/Scripts/custom.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function (){
+    $('#list-submit').on('click', function (e) {
+        e.preventDefault();
+
+        $.post("/Boards/CreateList",{})
+    });
+});

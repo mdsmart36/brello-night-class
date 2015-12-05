27a1c06e99fe6eeed90742121e69c6525ab1eee9
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2015-12-05 11:48:24 -0800

    Re-scaffolded InitialCreate Migration

--- Brello/Models/BrelloList.cs
+++ Brello/Models/BrelloList.cs
@@ -12,6 +12,12 @@ namespace Brello.Models
         public int BrelloListId { get; set; }
         public string Title { get; set; }
         public List<Card> Cards { get; set; }
+
+        // Required Annotation will ensure Entity returns a useful
+        // Validation message if CreateAt is null.
+        [Required]
         public DateTime CreatedAt { get; set; }
+
+        public virtual Board Board { get; set; }
     }
 }
